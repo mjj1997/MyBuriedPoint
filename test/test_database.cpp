ab5d65f3863fd0c6fc9860e7f40553c5129a566e
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2025-09-10 00:15:54 +0800

    test: reduce redudant test case to increase readability

--- test/test_database.cpp
+++ test/test_database.cpp
@@ -29,13 +29,8 @@ TEST_CASE("DatabaseTest", "[database]")
         REQUIRE(dataset.size() == i);
     }
 
-    // double-check priority to ensure insert(const Data&) work well
-    for (int i{ 0 }; i < limit; ++i) {
-        REQUIRE(dataset[i].priority == limit - i);
-    }
-
     // test remove(const Data&)
-    database.remove(dataset[0]);
+    database.remove(dataset.at(0));
     dataset = database.query(limit);
     REQUIRE(dataset.size() == 9);
 
@@ -44,17 +39,5 @@ TEST_CASE("DatabaseTest", "[database]")
     dataset = database.query(limit);
     REQUIRE(dataset.empty());
 
-    // test query(int32_t) to check whether it returns 10 limited data
-    for (int i{ 0 }; i < 2 * limit; ++i) {
-        const buried_point::Database::Data data{ .id = -1,
-                                                 .priority = i,
-                                                 .timestamp = static_cast<uint64_t>(i),
-                                                 .content = std::string{ "hello" } };
-        database.insert(data);
-    }
-    dataset = database.query(limit);
-    REQUIRE(dataset.size() == 10);
-
-    // remove database file
     std::filesystem::remove(dbPath);
 }
